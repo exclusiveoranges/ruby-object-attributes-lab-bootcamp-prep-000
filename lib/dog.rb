2b1d7b12fb1d88df11250c24f42df63fe4a4a061
Ryan O'Toole <otoolerj@gmail.com>
2018-03-19 17:45:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,5 @@
 class Dog
 
-
-
   def name
     @name
   end
@@ -9,4 +7,12 @@ class Dog
   def name=(new_name)
     @name = new_name
   end
-end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+end 
